7c912d9751cf2b9f5025cd3ce5488554a6ca2dbe
Luka Matovic <lukamatovic1994@gmail.com>
2024-11-29 05:24:42 -0600

    Adding CSS and more Weather API data

--- index.js
+++ index.js
@@ -27,11 +27,50 @@ app.post("/add", async (req, res) => {
     const lon = data[0].lon;
 
     const cityResponse = await axios.get(
-      `${URL}data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${apiKey}`
+      `${URL}data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${apiKey}&units=metric`
     );
-    console.log(cityResponse.data);
+
+    // Convert UTC time to the local time
+
+    const localCurrentTime = new Intl.DateTimeFormat("en-US", {
+      timeZone: cityResponse.data.timezone,
+      weekday: "long",
+      hour: "numeric",
+      minute: "numeric",
+    });
+
+    // Convert data to title case
+
+    const toTitleCase = (string) => {
+      return string
+        .split(" ")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" ");
+    };
+    console.log(cityResponse.data.daily);
+
+    res.render("index.ejs", {
+      data: cityResponse.data,
+      icon: cityResponse.data.current.weather[0].icon,
+      tempMetric: Math.round(cityResponse.data.current.temp),
+      humidity: cityResponse.data.current.humidity,
+      windMetric: (cityResponse.data.current.wind_speed * 3.6).toFixed(1),
+      city: toTitleCase(city),
+      description: toTitleCase(
+        cityResponse.data.current.weather[0].description
+      ),
+      time: localCurrentTime.format(),
+      feelsLike: Math.round(cityResponse.data.current.feels_like),
+      visibility: Math.round(cityResponse.data.current.visibility) / 1000,
+      precipitation: Math.round(cityResponse.data.hourly[0].pop * 10),
+      devPoint: Math.round(cityResponse.data.current.dew_point),
+    });
   } catch (error) {
-    console.log(error);
+    const err = "Location you entered does not exist. Please try again! ";
+    res.render("index.ejs", {error: err})
+
   }
 });
 // const apiKey = process.env.API_KEY;
